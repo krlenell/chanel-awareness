daf02c076c787b5d21622f8720e90fe03e96bfec
Kevin Lenell <krlenell@gmail.com>
2021-06-03 22:21:30 -0700

    cleanup and made GFM embed work

---
+++ src/components/GoFundMe.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="gfm-embed" data-url="https://www.gofundme.com/f/help-with-chanel-garcias-emergency-medical-bills/widget/large/">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GoFundMe',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-// import Vue from 'vue'
-// import App from './App.vue'
+import Vue from 'vue'
+import App from './App.vue'
 
-// Vue.config.productionTip = false
+Vue.config.productionTip = false
 
-// new Vue({
-//   render: h => h(App)
-// }).$mount('#app')
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
 
-window.onload = function () {
-  // similar behavior as an HTTP redirect
-  window.location.replace('https://gofund.me/c74e8676')
-}
+// window.onload = function () {
+//   // similar behavior as an HTTP redirect
+//   window.location.replace('https://gofund.me/c74e8676')
+// }
